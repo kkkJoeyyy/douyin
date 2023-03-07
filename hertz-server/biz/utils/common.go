d1640361210f2e4ae973508cc991b4f0a3d10958
2695915190@qq.com <kkkJoeyyy>
2023-03-08 03:47:02 +0800

    encrypt pws

--- hertz-server/biz/utils/common.go
+++ hertz-server/biz/utils/common.go
@@ -1,14 +1,13 @@
 package utils
 
 const (
-
 	MySQLDefaultDSN = "gorm:gorm@tcp(localhost:3306)/gorm?charset=utf8&parseTime=True&loc=Local"
 
-	//LocalURL     = "43.139.145.135:7777"
-	LocalURL     = "192.168.85.153:8888"
-	PlayURL      = "http://" + LocalURL + "/public/"
-	CoverTestURL = "http://" + LocalURL + "/public/cover/covertest.jpg"
-	CoverURL = "http://" + LocalURL + "/public/cover/"
-	AvatarTest = "http://" + LocalURL + "/public/avatar/"
+	LocalURL = "43.139.145.135:7777"
+	//LocalURL     = "192.168.85.153:8888"
+	PlayURL        = "http://" + LocalURL + "/public/"
+	CoverTestURL   = "http://" + LocalURL + "/public/cover/covertest.jpg"
+	CoverURL       = "http://" + LocalURL + "/public/cover/"
+	AvatarTest     = "http://" + LocalURL + "/public/avatar/"
 	BackgroundTest = "http://" + LocalURL + "/public/background/"
 )
